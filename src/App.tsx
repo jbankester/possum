657955c31ecc70729a9cbc22b6c902e565c60c1a
Jacob Bankester <jacob.bankester@gmail.com>
2024-05-12 23:07:12 -0500

    make a drawer

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Drawer from "./components/drawer/Drawer";
+
+function App() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const doli = () => {
+    let lis = [];
+    for (let x = 0; x < 100; x++) {
+      lis.push(<li>{x}</li>);
+    }
+    return lis;
+  };
+  return (
+    <>
+      <button onClick={() => setDrawerOpen(true)}>Open Drawer</button>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <ul>{doli()}</ul>
+      </div>
+      <Drawer
+        isOpen={drawerOpen}
+        onClose={() => {
+          console.log("on close");
+          setDrawerOpen(false);
+        }}
+      >
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div>this is drawer content</div>
+          <input type="text"></input>
+          <button>button</button>
+        </div>
+      </Drawer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/drawer/Drawer.tsx
@@ -0,0 +1,56 @@
+import { useRef, useEffect } from "react";
+import "./Drawer.css";
+
+interface DrawerProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+  width?: string;
+}
+
+const Drawer = ({ isOpen, onClose, children, width }: DrawerProps) => {
+  const bodyRef = useRef(document.querySelector("body"));
+  const closeOnEsc = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      console.log("esc");
+      onClose();
+    }
+  };
+  //close drawer on esc
+  useEffect(() => {
+    document.addEventListener("keydown", closeOnEsc, false);
+    return () => document.removeEventListener("keydown", closeOnEsc, false);
+  }, []);
+
+  //dont allow body to scroll when drawer open
+  useEffect(() => {
+    const toggleBodyScroll = () => {
+      if (isOpen) {
+        bodyRef.current!.style.overflow = "hidden";
+      } else {
+        bodyRef.current!.style.overflow = "";
+      }
+    };
+    toggleBodyScroll();
+  }, [isOpen]);
+
+  return (
+    <div className={`container`}>
+      <div className={`content ${isOpen ? "opened" : "closed"}`}>
+        <div>
+          header <button onClick={onClose}>Close Drawer</button>
+        </div>
+        {children}
+      </div>
+      <div
+        className={`backdrop ${isOpen ? "opened" : "display-none"}`}
+        onClick={() => {
+          console.log("close");
+          onClose();
+        }}
+      />
+    </div>
+  );
+};
+
+export default Drawer;
